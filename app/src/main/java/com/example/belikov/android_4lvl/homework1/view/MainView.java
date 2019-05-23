fe54d9c0ca74d753b39538df58a1652907a5f131
Belikov <Sergeybelik86@gmail.com>
2019-05-23 16:22:42 +0400

    ДЗ №1, п.2

--- app/src/main/java/com/example/belikov/android_4lvl/homework1/view/MainView.java
+++ app/src/main/java/com/example/belikov/android_4lvl/homework1/view/MainView.java
@@ -3,6 +3,5 @@ package com.example.belikov.android_4lvl.homework1.view;
 import android.view.View;
 
 public interface MainView {
-
-    public void setTextView(String s);
+    void setTextView(String s);
 }
