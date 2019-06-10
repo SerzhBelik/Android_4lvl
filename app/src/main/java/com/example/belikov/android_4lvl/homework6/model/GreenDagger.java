be3bd4202562661fb857f410c074e3fc6ebc9530
Belikov <Sergeybelik86@gmail.com>
2019-06-10 16:42:51 +0400

    ДЗ №6 n.3

---
+++ app/src/main/java/com/example/belikov/android_4lvl/homework6/model/GreenDagger.java
@@ -0,0 +1,10 @@
+package com.example.belikov.android_4lvl.homework6.model;
+
+public class GreenDagger {
+
+    private String s = "green";
+
+    public String show(){
+        return s;
+    }
+}
---
+++ app/src/main/java/com/example/belikov/android_4lvl/homework6/presenter/DaggerPresenter.java
@@ -0,0 +1,39 @@
+package com.example.belikov.android_4lvl.homework6.presenter;
+
+
+import android.util.Log;
+
+import com.arellomobile.mvp.InjectViewState;
+import com.arellomobile.mvp.MvpPresenter;
+import com.example.belikov.android_4lvl.homework6.app.App;
+import com.example.belikov.android_4lvl.homework6.model.GreenDagger;
+import com.example.belikov.android_4lvl.homework6.model.RedDagger;
+import com.example.belikov.android_4lvl.homework6.model.WhiteDagger;
+import com.example.belikov.android_4lvl.homework6.view.NoDiView;
+
+import javax.inject.Inject;
+
+@InjectViewState
+public class DaggerPresenter extends MvpPresenter<NoDiView> {
+
+    private static final String TAG = "DaggerPresenter";
+//    private DaggerPresenter presenter;
+
+    @Inject
+    GreenDagger green;
+    @Inject
+    RedDagger red;
+    @Inject
+    WhiteDagger white;
+
+    public DaggerPresenter(){
+
+    }
+
+    public void show() {
+        Log.d(TAG, this.toString());
+        App.getAppComponent().inject(this);
+        getViewState().setText(green.show());
+
+    }
+}
